ec4c5b563268ddb5794cfb14128bdd2c81a3311d
Ahmed Mohsen Hassan <ahmedmohsen106@gmail.com>
2015-01-27 22:52:31 +0200

    Update

--- MoveToFront/MTF.cs
+++ MoveToFront/MTF.cs
@@ -11,38 +11,39 @@ namespace MoveToFront
         public List<int> Initialization()
         {
             List<int> work = new List<int>();
-            for (int ind = 0; ind < 257; ind++)
+            for (int ind = 0; ind < 257; ind++) //O(1)
             {
                 work.Add(ind);
             }
             return work;
         }
-        public List<int> Encoding(string input)
+        public List<int> Encoding(string input) //O(n)
         {
             List<int> output = new List<int>();
             List<int> work = Initialization();
-            for(int ind = 0; ind < input.Length; ind ++)
+            for (int ind = 0; ind < input.Length; ind++) //O(n)
             {
                 int t = (int)input[ind];
                 int target = work.IndexOf(t);
-                output.Add(target);
-                work.Remove(input[ind]);
-                work.Insert(0, (int)input[ind]);
+                output.Add(target); //O(1)
+                work.Remove(input[ind]); //O(1)
+                work.Insert(0, (int)input[ind]); //O(1)
                
             }
             return output;
         }
         public string Decoding(List<int> input)
         {
-            string output = "";
+            string output = input.ToString(); //O(n)
             List<int> work = Initialization();
-            for (int ind = 0; ind < input.Count; ind++)
+            int Length = input.Count;
+            for (int ind = 0; ind < Length; ind++) //O(n)
             {
                 int t = input[ind];
-                int target = work.ElementAt(t);
+                int target = work[t];
                 output += (char)target;
-                work.Remove(target);
-                work.Insert(0, target);
+                work.Remove(target); //O(1)
+                work.Insert(0, target); //O(1)
             }
             return output;
         }
